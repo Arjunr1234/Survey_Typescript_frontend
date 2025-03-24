3e2812e8b5ae44cae022d3678e2b65d5117e8768
Arjunr1234 <arjunreji1234@gmail.com>
2025-03-24 14:46:42 +0530

    changed the base url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-//const BASE_URL = "https://surveyapp-backend-xapd.onrender.com"; 
-const BASE_URL = "http://localhost:5002"; 
+
+const BASE_URL = "https://survey-typescript-backend.onrender.com"
+//const BASE_URL = "http://localhost:5002"; 
 //const BASE_URL = import.meta.env.VITE_URL
 
 export const axiosInstance = axios.create({
